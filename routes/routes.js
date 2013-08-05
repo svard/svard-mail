60e1599da0fc0a3ea76c7c7dd41715305aadc132
Kristofer Svärd <kristofer.svard@gmail.com>
2013-08-05 21:20:26 +0200

    Added validator to /headers route.

--- routes/routes.js
+++ routes/routes.js
@@ -12,40 +12,28 @@ module.exports = function(app, logger) {
 
     app.get('/headers/start/:from/count/:count', function(req, resp) {
         var from = parseInt(req.params.from, 10),
-            count = parseInt(req.params.count, 10);
+            count = parseInt(req.params.count, 10),
+            preCond = utils.condition(utils.validator('can not be zero', utils.complement(utils.zero)),
+                                      utils.validator('can not be negative', utils.complement(utils.negative))),
+            checkedGetHeaders = _.partial(preCond, Mail.getHeaders);
 
         try {
-            if(utils.zero(from)) {
-                throw new Error('from can not be zero');
-            }
-            if(utils.negative(from)) {
-                throw new Error('from can not be negative');
-            }
-            if(utils.zero(count)) {
-                throw new Error('count can not be zero');
-            }
-            if(utils.negative(count)) {
-                throw new Error('count can not be negative');
-            }
+            checkedGetHeaders(from, count).then(function(headers) {
+                resp.send(headers);
+            }, function(err) {
+                logger.error('Can\'t fetch headers: %s, from: %d, count: %d:', err.message, from, count);
+                resp.send(500);
+            });
         } catch(e) {
             logger.error(e.message);
             resp.send(500);
-            return;
         }
-
-        Mail.getHeaders(from, count).then(function(headers) {
-            resp.send(headers);
-        }, function(err) {
-            logger.error('Can\'t fetch headers: %s, from: %d, count: %d:', err.message, from, count);
-            resp.send(500);
-        });
     });
 
     app.get('/message/:uid', function(req, resp) {
         var uid = parseInt(req.params.uid, 10),
-            preCond = utils.condition(utils.validator('uid must be a number', _.isNumber),
-                                      utils.validator('uid can not be zero', utils.complement(utils.zero)),
-                                      utils.validator('uid can not be negative', utils.complement(utils.negative))),
+            preCond = utils.condition(utils.validator('can not be zero', utils.complement(utils.zero)),
+                                      utils.validator('can not be negative', utils.complement(utils.negative))),
             checkedGetOneMessage = _.partial(preCond, Mail.getOneMessage);
 
         try {
