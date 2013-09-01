35d95418b4e21292f2409376279c7db8fb3b0dd1
Kristofer Svärd <kristofer.svard@gmail.com>
2013-09-01 19:04:16 +0200

    New directive test cases and bug fixes.

--- karma.conf.js
+++ karma.conf.js
@@ -1,50 +1,35 @@
 // Karma configuration
 
 // base path, that will be used to resolve files and exclude
-basePath = '';
+basePath = 'app';
+
+preprocessors = {
+  'views/templates/*.html': 'html2js'
+};
 
 // list of files / patterns to load in the browser
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'app/components/angular/angular.js',
-  'app/components/angular-resource/angular-resource.js',
-  'app/components/angular-mocks/angular-mocks.js',
-  'app/components/angular-route/angular-route.js',
-  'app/components/underscore/underscore.js',
-  'app/scripts/*.js',
-  'app/scripts/**/*.js',
-  'app/views/templates/*.html',
-  'test/mock/**/*.js',
-  'test/spec/**/*.js',
+  'components/jquery/jquery.js',
+  'components/angular/angular.js',
+  'components/angular-resource/angular-resource.js',
+  'components/angular-mocks/angular-mocks.js',
+  'components/angular-route/angular-route.js',
+  'components/underscore/underscore.js',
+  'scripts/*.js',
+  'scripts/**/*.js',
+  'views/templates/*.html',
+  '../test/mock/**/*.js',
+  '../test/spec/**/*.js'
 ];
-
-preprocessors = {
-  'app/views/templates/*.html': 'html2js'
-};
-
-ngHtml2JsPreprocessor = {
-  // strip this from the file path
-  stripPrefix: 'app/',
-  // prepend this to the
-  // prependPrefix: 'app/',
-
-  // or define a custom transform function
-  // cacheIdFromPath: function(filepath) {
-  //   return cacheId;
-  // },
-
-  // setting this option will create only a single module that contains templates
-  // from all the files, so you can load them all with module('foo')
-  // moduleName: 'foo'
-}
 
 // list of files to exclude
 exclude = [];
 
 // test results reporter to use
 // possible values: dots || progress || growl
-reporters = ['progress'];
+reporters = ['dots', 'junit'];
 
 // web server port
 port = 8585;
@@ -78,7 +63,6 @@ captureTimeout = 5000;
 // Continuous Integration mode
 // if true, it capture browsers, run tests and exit
 singleRun = true;
-reporters = ['dots', 'junit'];
 junitReporter = {
   outputFile: 'test-results.xml'
 };
--- test/spec/directives/tableDirectives.js
+++ test/spec/directives/tableDirectives.js
@@ -1,64 +1,87 @@
-// 'use strict';
+'use strict';
 
-// describe('TableDirectives', function () {
+describe('TableDirectives', function () {
 
-//     // load the directive's module
-//     beforeEach(module('svardMailApp'));
+    // load the directive's module
+    beforeEach(module('svardMailApp'));
 
-//     describe('mailTable', function() {
+    describe('mailTable', function() {
 
-//         var element,
-//             scope;
+        var element,
+            scope;
 
-//         beforeEach(module('app/views/templates/mailTable.html', 'app/views/templates/mailTableRow.html'));
+        beforeEach(module('views/templates/mailTable.html'));
 
-//         beforeEach(inject(function ($rootScope, $compile) {
-//             scope = $rootScope.$new();
-//             scope.box = {
-//                 headers: [{
-//                     "seqno": 8,
-//                     "uid": 8,
-//                     "flags": ["\\Seen"],
-//                     "date": "2013-07-21T15:33:14.000Z",
-//                     "subject": "testar",
-//                     "from": [{
-//                         "address": "kristofer.svard@gmail.com",
-//                         "name": "Kristofer Svärd"
-//                     }],
-//                     "text": "My name is Kristofer"
-//                 }, {
-//                     "seqno": 9,
-//                     "uid": 9,
-//                     "flags": ["\\Seen"],
-//                     "date": "2013-07-22T06:18:56.000Z",
-//                     "subject": "Hello world",
-//                     "from": [{
-//                         "address": "kristofer.svard@ericsson.com",
-//                         "name": "Kristofer Svärd"
-//                     }],
-//                     "text": "Hi there\n\nThis is hello world!\n"
-//                 }, {
-//                     "seqno": 10,
-//                     "uid": 10,
-//                     "flags": ["\\Unseen"],
-//                     "date": "2013-08-06T23:35:15.000Z",
-//                     "subject": "Hello",
-//                     "from": [{
-//                         "address": "annieh310@yahoo.com.cn",
-//                         "name": "Annie Huang"
-//                     }],
-//                     "text": "Test\n"
-//                 }]
-//             };
-//             element = angular.element('<table class="ink-table" mail-table headers="box.headers"></table>');
-//             element = $compile(element)(scope);
-//             scope.$digest();
-//         }));
+        beforeEach(inject(function ($rootScope, $compile) {
+            scope = $rootScope.$new();
+            scope.box = {
+                headers: [{
+                    "seqno": 8,
+                    "uid": 8,
+                    "flags": ["\\Seen"],
+                    "date": "2013-07-21T15:33:14.000Z",
+                    "subject": "testar",
+                    "from": [{
+                        "address": "kristofer.svard@gmail.com",
+                        "name": "Kristofer Svärd"
+                    }],
+                    "text": "My name is Kristofer"
+                }, {
+                    "seqno": 9,
+                    "uid": 9,
+                    "flags": ["\\Seen"],
+                    "date": "2013-07-22T06:18:56.000Z",
+                    "subject": "Hello world",
+                    "from": [{
+                        "address": "kristofer.svard@ericsson.com",
+                        "name": "Kristofer Svärd"
+                    }],
+                    "text": "Hi there\n\nThis is hello world!\n"
+                }, {
+                    "seqno": 10,
+                    "uid": 10,
+                    "flags": ["\\Unseen"],
+                    "date": "2013-08-06T23:35:15.000Z",
+                    "subject": "Hello",
+                    "from": [{
+                        "address": "annieh310@yahoo.com.cn",
+                        "name": "Annie Huang"
+                    }],
+                    "text": "Test\n"
+                }]
+            };
+            element = angular.element('<table mail-table headers="box.headers"></table>');
+            element = $compile(element)(scope);
+            scope.$digest();
+        }));
 
-//         it('should create a 3 rows in the table', inject(function ($compile) {
-//             var rows = element.find('tr');
+        it('should create a 3 rows in the table', function () {
+            var rows = element.find('tr');
 
-//             expect(rows.length).toBe(7); // 1 header row + 3 rows + 3 body text rows
-//         }));
-//     });
-// });
+            expect(rows.length).toBe(7); // 1 header row + 3 rows + 3 body text rows
+        });
+
+        it('should display one mail as unseen', function () {
+            var unseenRow = element.find('.svard-mail-unseen');
+
+            expect(unseenRow.length).toBe(1);
+        });
+
+        it('should toggle visibility of the body text', function () {
+            var clickedRow = element.find('tbody tr button').children().eq(0),
+                visibleBody = element.find('.svard-mail-hide');
+
+            expect(visibleBody.length).toBe(3);
+
+            clickedRow.click();
+
+            visibleBody = element.find('.svard-mail-hide');
+            expect(visibleBody.length).toBe(2);
+
+            clickedRow.click();
+
+            visibleBody = element.find('.svard-mail-hide');
+            expect(visibleBody.length).toBe(3);
+        });
+    });
+});
