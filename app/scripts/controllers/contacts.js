7b181129bc8903bcbc1391df336335305bb33f08
Kristofer Svärd <kristofer.svard@gmail.com>
2013-09-08 22:00:23 +0200

    Started work on the contacts view.

--- app/scripts/controllers/contacts.js
+++ app/scripts/controllers/contacts.js
@@ -1,10 +1,16 @@
 'use strict';
 
 angular.module('svardMailApp')
-  .controller('ContactsCtrl', ['$scope', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  }]);
+    .controller('ContactsCtrl', ['$scope', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma',
+            'My own stuff',
+            'Another thing'
+        ];
+
+        $scope.addContact = function () {
+            $scope.awesomeThings.push('New thing');
+        };
+    }]);
