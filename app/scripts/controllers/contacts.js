c888d98b0d777482747ba4b4878d7dda387357f3
Kristofer Svärd <kristofer.svard@gmail.com>
2013-09-15 21:27:23 +0200

    Added action menu to contacts view

--- app/scripts/controllers/contacts.js
+++ app/scripts/controllers/contacts.js
@@ -2,15 +2,31 @@
 
 angular.module('svardMailApp')
     .controller('ContactsCtrl', ['$scope', function ($scope) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma',
-            'My own stuff',
-            'Another thing'
+        $scope.contactId = 1;
+        $scope.contacts = [
+            {name: 'Annie Huang', address: 'juan.svard@gmail.com', id: $scope.contactId++},
+            {name: 'Kristofer Svärd', address: 'kristofer.svard@gmail.com', id: $scope.contactId++}
         ];
 
         $scope.addContact = function () {
-            $scope.awesomeThings.push('New thing');
+            $scope.contacts.push({name: '', address: '', id: $scope.contactId++});
+        };
+    }])
+
+    .controller('ContactCtrl', ['$scope', function ($scope) {
+        $scope.actionMenuVisible = false;
+        $scope.actionMenuStyle = {display: 'none'};
+
+        $scope.toggleActionMenu = function () {
+            $scope.actionMenuVisible = !$scope.actionMenuVisible;
+            if ($scope.actionMenuVisible) {
+                $scope.actionMenuStyle.display = 'block';
+            } else {
+                $scope.actionMenuStyle.display = 'none';
+            }
+        };
+
+        $scope.deleteContact = function (index) {
+            $scope.contacts.splice(index, 1);
         };
     }]);
--- test/spec/controllers/contacts.js
+++ test/spec/controllers/contacts.js
@@ -6,7 +6,7 @@ xdescribe('Controller: ContactsCtrl', function () {
     beforeEach(module('svardMailApp'));
 
     var ContactsCtrl,
-    scope;
+        scope;
 
     // Initialize the controller and a mock scope
     beforeEach(inject(function ($controller, $rootScope) {
@@ -18,5 +18,49 @@ xdescribe('Controller: ContactsCtrl', function () {
 
     it('should attach a list of awesomeThings to the scope', function () {
         expect(scope.awesomeThings.length).toBe(3);
+    });
+});
+
+describe('Controller: ContactCtrl', function () {
+    // load the controller's module
+    beforeEach(module('svardMailApp'));
+
+    var ContactCtrl,
+        scope;
+
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        scope.contactId = 0;
+        scope.actionMenuVisible = false;
+        scope.actionMenuStyle = {display: 'none'};
+        scope.contacts = [
+            {name: 'Annie Huang', address: 'juan.svard@gmail.com', id: scope.contactId++},
+            {name: 'Kristofer Svärd', address: 'kristofer.svard@gmail.com', id: scope.contactId++}
+        ];
+        ContactCtrl = $controller('ContactCtrl', {
+            $scope: scope
+        });
+    }));
+
+    it('should delete one contact', function () {
+        expect(scope.contacts.length).toBe(2);
+        scope.deleteContact(0);
+        expect(scope.contacts.length).toBe(1);
+    });
+
+    it('should toggle the action menu', function () {
+        expect(scope.actionMenuVisible).toBe(false);
+        expect(scope.actionMenuStyle.display).toEqual('none');
+
+        scope.toggleActionMenu();
+
+        expect(scope.actionMenuVisible).toBe(true);
+        expect(scope.actionMenuStyle.display).toEqual('block');
+
+        scope.toggleActionMenu();
+
+        expect(scope.actionMenuVisible).toBe(false);
+        expect(scope.actionMenuStyle.display).toEqual('none');
     });
 });
