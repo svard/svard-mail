60914de893489b8ad67c4862d0d67c210348cf6e
Kristofer Svärd <kristofer.svard@gmail.com>
2013-11-18 18:22:01 +0100

    New loader animation
    Changed the loader animation to a rotating circle.

--- app/scripts/directives/loaderDirectives.js
+++ app/scripts/directives/loaderDirectives.js
@@ -1,18 +1,9 @@
 'use strict';
 
 angular.module('svardMailApp')
-    .directive('loader', ['$rootScope', function ($rootScope) {
+    .directive('loader', ['$rootScope', '$window', function ($rootScope, $window) {
         return {
-            template: '<div class="svard-mail-loader">' +
-                    '<h2>Loading</h2>' +
-                    '<span></span>' +
-                    '<span></span>' +
-                    '<span></span>' +
-                    '<span></span>' +
-                    '<span></span>' +
-                    '<span></span>' +
-                    '<span></span>' +
-                '</div>',
+            template: '<div class="loader loader-2" ng-style="getViewportCenter()"></div>',
             restrict: 'E',
             replace: true,
             link: function postLink(scope, element) {
@@ -25,6 +16,16 @@ angular.module('svardMailApp')
                 $rootScope.$on('$routeChangeSuccess', function () {
                     element.addClass('svard-mail-hide');
                 });
+
+                scope.getViewportCenter = function () {
+                    var top = ($window.innerHeight / 2) - 20,
+                        left = ($window.innerWidth / 2) - 20;
+
+                    return {
+                        top: top + 'px',
+                        left: left + 'px'
+                    };
+                };
             }
         };
     }]);
